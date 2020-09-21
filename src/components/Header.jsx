b77848d0406ee92d6203e6fbb05e33d85f767a2c
Shahid Mehmood <iss_shahid@yahoo.com>
2020-09-21 19:07:41 +0500

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,6 +13,7 @@ function Header() {
         <img
           className="header__logo"
           src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
         />
       </Link>
       <div className="header__search">
